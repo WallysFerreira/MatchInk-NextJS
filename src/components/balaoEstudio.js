329b475f4be9f9e5b2cadccfc04103f88fee1156
Wallys Ferreira <wallysfsilva@gmail.com>
2023-05-30 18:44:37 -0300

    atualizar modulo balaoEstudio

--- src/components/balaoEstudio.js
+++ src/components/balaoEstudio.js
@@ -1,4 +1,15 @@
 export default function BalaoEstudio() {
   return (
+    <section>
+      <div>
+        <p>Studio Recife <br />
+        Recife |SEG - DOM <br />
+        08h às 19h</p>
+      </div>
+
+      <div id="pontos">
+        <h1>9.2/10</h1>
+      </div>
+    </section>
   );
 }

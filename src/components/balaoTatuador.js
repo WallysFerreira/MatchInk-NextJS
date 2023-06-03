43f34a73aa301638f987f7dc543d77ddb4c29b37
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-03 09:00:06 -0300

    estilizar balao tatuador

--- src/components/balaoTatuador.js
+++ src/components/balaoTatuador.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import '../style/Perfil.module.css';
+import styles from '../style/PerfilTatuador.module.css';
 
 export default function BalaoTatuador(props) {
   const tatuador = props.tatuador;
@@ -7,19 +7,21 @@ export default function BalaoTatuador(props) {
 
   return (
     <section>
-      <div>
-        <a> <Image src="/Tela6/Instagram.svg" alt="icon instagram" width="70" height="70" /> </a>
-      </div>
+      <div className={styles.container}>
+        <a> <Image src="/Tela6/instagramVermelho.svg" alt="icon instagram" width="21" height="21" /> </a>
 
-      <div id="bio">
-        <Image id="fotoperfil" src={tatuador.url_foto_perfil} alt="Foto do perfil" width="300" height="300" />
-        <h2>{tatuador.nome}</h2>
-        <p>{tatuador.bio}</p>
-      </div>
+        <div className={styles.meio}>
+          <Image className={styles.fotoPerfil} src={tatuador.url_foto_perfil} alt="Foto do perfil" width="160" height="160" />
+          <div className={styles.informacoes}>
+            <h2>{tatuador.nome}</h2>
+            <p>{tatuador.bio}</p>
+          </div>
+        </div>
 
-      <div id="iconcm">
-        <a> <Image src="/Tela6/calendarioPreto.svg" alt="icon calendario" width="20" height="20" /> </a>
-        <a> <Image src="/Tela6/mensagemPreto.svg" alt="icon mensagem" width="20" height="20" /> </a>
+        <div className={styles.icones}>
+          <a> <Image src="/Tela6/calendarioPreto.svg" alt="icon calendario" width="20" height="20" /> </a>
+          <a> <Image src="/Tela6/mensagemPreto.svg" alt="icon mensagem" width="20" height="20" /> </a>
+        </div>
       </div>
     </section>
   );
--- src/pages/tatuador/[id].js
+++ src/pages/tatuador/[id].js
@@ -2,6 +2,7 @@ import Rodape from "../../components/rodape";
 import BalaoTatuador from "../../components/balaoTatuador";
 import Portfolio from "../../components/portfolio";
 import BalaoEstudio from "../../components/balaoEstudio";
+import Image from "next/image";
 
 export async function getServerSideProps({ params }) {
   const id = params.id;
@@ -16,9 +17,9 @@ export async function getServerSideProps({ params }) {
 }
 
 export default function Usuario({ tatuador }) {
-  console.log(tatuador);
   return (
     <main>
+      <Image src="/Tela6/Capa.png" alt="" width="412" height="220" />
       <BalaoTatuador tatuador={tatuador} />
       <BalaoEstudio />
       <Portfolio />

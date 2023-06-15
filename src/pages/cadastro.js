e0ed4e99fef696a8609508635058543b230b818a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-15 16:04:33 -0300

    adicionar rodape na tela de cadastro e criar pagina estudios

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,9 +1,11 @@
+import Rodape from "../components/rodape";
 import FormularioCadastro from "../components/formularioCadastro";
 
 export default function Cadastro() {
   return (
     <main>
       <FormularioCadastro />
+      <Rodape />
     </main>
   );
 }

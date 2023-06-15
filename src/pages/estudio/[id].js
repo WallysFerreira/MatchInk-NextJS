2c03f786af8200d65c77259ac873fcb6eb0dbdd8
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-15 17:22:47 -0300

    começar a fazer tela de perfil do estudio

--- src/pages/estudio/[id].js
+++ src/pages/estudio/[id].js
@@ -1,3 +1,7 @@
+import BalaoEstudio from "../../components/balaoEstudio";
+import Rodape from "../../components/rodape";
+import styles from '../../style/PerfilEstudio.module.css';
+
 export async function getServerSideProps({ params }) {
   const id = params.id;
   const res = await fetch(`http://localhost:8080/estudios/${id}`);
@@ -11,5 +15,14 @@ export async function getServerSideProps({ params }) {
 }
 
 export default function Estudio({ estudio }) {
-
+  return (
+    <main>
+      <div className={styles.fotos}>
+        <div className={styles.nota}>
+          <h1>{estudio.nota}/10</h1>
+        </div>
+      </div>
+      <Rodape />
+    </main>
+  );
 }
